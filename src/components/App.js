5ffcea1a706e96ff7380e35fb980c60563fde3bb
Thomas Angelopoulos <thomas.angelopoulos@rwc01-lap01-026.zazzle.com>
2020-01-29 13:10:09 -0800

    create coming soon page

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 import ImageList from "./ImageList";
+import ComingSoon from "./ComingSoon";
+import CustomData from "../data/customData";
 
 class App extends React.Component {
-  state = { images: [] };
+  state = {
+    images: [CustomData]
+  };
 
   onSearchSubmit = async term => {
     const response = await unsplash.get("/search/photos", {
@@ -12,12 +16,14 @@ class App extends React.Component {
     });
 
     this.setState({ images: response.data.results });
+    console.log(response.data.results);
   };
 
   render() {
+    console.log(this.state);
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ComingSoon />
         <ImageList images={this.state.images} />
       </div>
     );
---
+++ src/components/ComingSoon.js
@@ -0,0 +1,14 @@
+import "./ComingSoon.css";
+import React from "react";
+
+class ComingSoon extends React.Component {
+  render() {
+    return (
+      <div className="coming-soon">
+        <h1>Coming Soon!</h1>
+      </div>
+    );
+  }
+}
+
+export default ComingSoon;
